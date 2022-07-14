6472e7b6a8cce73b8be41bc872faa717de2ab317
Ale Aguiar <105741238+aleaguiar13@users.noreply.github.com>
2022-07-14 18:31:40 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Appdesafioreact extends Component {
+  render() {
+    return <h1>desafio react feito!</h1>;
+  }
+}
+export default Appdesafioreact;
